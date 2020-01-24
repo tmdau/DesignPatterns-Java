79900bc8b66f692f890f4f43b318dff9325043f8
Timur Dautov <tmdautov@gmail.com>
2020-01-24 08:16:57 +0600

    Update state.ts

--- behavioral/state.ts
+++ behavioral/state.ts
@@ -1,30 +1,16 @@
-/**
- * The Context defines the interface of interest to clients. It also maintains a
- * reference to an instance of a State subclass, which represents the current
- * state of the Context.
- */
 class Context {
-    /**
-     * @type {State} A reference to the current state of the Context.
-     */
     private state: State;
 
     constructor(state: State) {
         this.transitionTo(state);
     }
 
-    /**
-     * The Context allows changing the State object at runtime.
-     */
     public transitionTo(state: State): void {
         console.log(`Context: Transition to ${(<any>state).constructor.name}.`);
         this.state = state;
         this.state.setContext(this);
     }
 
-    /**
-     * The Context delegates part of its behavior to the current State object.
-     */
     public request1(): void {
         this.state.handle1();
     }
@@ -34,12 +20,6 @@ class Context {
     }
 }
 
-/**
- * The base State class declares methods that all Concrete State should
- * implement and also provides a backreference to the Context object, associated
- * with the State. This backreference can be used by States to transition the
- * Context to another State.
- */
 abstract class State {
     protected context: Context;
 
@@ -52,10 +32,6 @@ abstract class State {
     public abstract handle2(): void;
 }
 
-/**
- * Concrete States implement various behaviors, associated with a state of the
- * Context.
- */
 class ConcreteStateA extends State {
     public handle1(): void {
         console.log('ConcreteStateA handles request1.');
